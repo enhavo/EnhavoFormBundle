c5d7903f254c43508b17ef0d890d711e2f57fb5c
Gerhard Seidel <gseidel.message@googlemail.com>
2023-01-11 17:16:00 +0100

    Form event dispatcher (#1707)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | yes
    | New feature?  | yes
    | Doc PR?       | no
    | Backport      | no
    | License       | MIT
    
    * Add form event dispatcher
    * Add Wysiwyg form
    * Create registered form models on factory create
    * Remove `RootForm`
    * Inject event dispatcher to every form model
    * Dispatch change events

---
+++ Form/VueType/WysiwygVueType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Enhavo\Bundle\FormBundle\Form\VueType;
+
+use Enhavo\Bundle\VueFormBundle\Form\AbstractVueType;
+use Enhavo\Bundle\VueFormBundle\Form\VueData;
+use Symfony\Component\Form\FormView;
+
+class WysiwygVueType extends AbstractVueType
+{
+    public static function supports(FormView $formView): bool
+    {
+        return in_array('enhavo_wysiwyg', $formView->vars['block_prefixes']);
+    }
+
+    public function buildView(FormView $view, VueData $data)
+    {
+        $data['component'] = 'form-wysiwyg';
+    }
+}
